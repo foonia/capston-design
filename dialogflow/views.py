d23bd13fa240c3eccce7be15a0c9a6739ca5ac41
foonia <ljh9032a@naver.com>
2018-09-23 14:39:51 +0900

    csrf_exempt 예외 처리 추가

--- dialogflow/views.py
+++ dialogflow/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http.response import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 
+@csrf_exempt
 def fulfillment(request):
     print(request.JSON)
 
